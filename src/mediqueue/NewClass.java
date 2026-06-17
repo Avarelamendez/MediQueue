1a99ac783c3c3bb4c18901386363ba8e2b3d527a
Avarelamendez <avarelamendez@gmail.com>
2026-06-16 21:48:25 -0600

    prueba

---
+++ src/mediqueue/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mediqueue;
+
+/**
+ *
+ * @author AMD
+ */
+public class NewClass {
+    
+}
